2794cb1103cb5e43191f449b45a0a2413254e254
Antoine Motet <motet_a@epitech.eu>
2015-12-11 11:22:04 +0100

    Added tests for StringReader

--- test/string_reader.c
+++ test/string_reader.c
@@ -6,21 +6,39 @@
 #include "../string_reader.h"
 
 
-static void base(void) {
+static void empty(void) {
   degat_StringReader *reader;
 
   reader = degat_StringReader_newStr("");
   yolo_assert(!degat_StringReader_hasMore(reader));
+  yolo_assert_int_eq(1, reader->position.line);
+  yolo_assert_int_eq(0, reader->position.column);
   degat_StringReader_delete(reader);
+}
 
-  reader = degat_StringReader_newStr("a");
+static void newLine(void) {
+  degat_StringReader *reader;
+
+  reader = degat_StringReader_newStr("a\n");
+  yolo_assert_int_eq(1, reader->position.line);
+  yolo_assert_int_eq(0, reader->position.column);
   yolo_assert(degat_StringReader_hasMore(reader));
-  yolo_assert_char_eq(degat_StringReader_next(reader), 'a');
+
+  yolo_assert_char_eq('a', degat_StringReader_next(reader));
+  yolo_assert(degat_StringReader_hasMore(reader));
+  yolo_assert_int_eq(1, reader->position.line);
+  yolo_assert_int_eq(1, reader->position.column);
+
+  yolo_assert_char_eq('\n', degat_StringReader_next(reader));
   yolo_assert(!degat_StringReader_hasMore(reader));
+  yolo_assert_int_eq(2, reader->position.line);
+  yolo_assert_int_eq(0, reader->position.column);
+
   degat_StringReader_delete(reader);
 }
 
 
 void stringReaderTestSuite(void) {
-  base();
+  empty();
+  newLine();
 }
