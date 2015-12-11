54fbbc88c96259082f111e5e7db0c661225eb83b
Antoine Motet <motet_a@epitech.eu>
2015-12-11 16:41:21 +0100

    Added doc in object.h

--- object.h
+++ object.h
@@ -4,9 +4,12 @@
 #include <stdint.h>
 #include "class.h"
 
+/**
+ * This macro must be included at the first line of any object structure.
+ */
 # define wsky_OBJECT_HEAD \
   uint32_t gcReferenceCount; \
-  const wsky_Class * class;
+  const wsky_Class *class;
 
 
 
@@ -19,20 +22,32 @@ extern const wsky_Class wsky_Object_CLASS;
 
 
 wsky_ReturnValue wsky_Object_new(const wsky_Class *class,
-				   unsigned paramCount,
-				   wsky_Value *params);
+				 unsigned paramCount,
+				 wsky_Value *params);
 
+/** Returns the `Method` associated with the given name or `NULL` */
 const wsky_Method *wsky_Object_findMethod(wsky_Object *object,
-					    const char *methodName);
+					  const char *methodName);
 
+/** Calls a method */
 wsky_ReturnValue wsky_Object_callMethod(wsky_Object *object,
-					  const char *methodName,
-					  unsigned parameterCount,
-					  wsky_Value *parameters);
+					const char *methodName,
+					unsigned parameterCount,
+					wsky_Value *parameters);
+
+/** Calls a method without parameter */
 wsky_ReturnValue wsky_Object_callMethod0(wsky_Object *object,
-					   const char *methodName);
+					 const char *methodName);
+
+/** Calls a method with one parameter */
 wsky_ReturnValue wsky_Object_callMethod1(wsky_Object *object,
-					   const char *methodName,
-					   wsky_Value a);
+					 const char *methodName,
+					 wsky_Value a);
+
+/** Calls a method with two parameters */
+wsky_ReturnValue wsky_Object_callMethod2(wsky_Object *object,
+					 const char *methodName,
+					 wsky_Value a,
+					 wsky_Value b);
 
 #endif /* !OBJECT_H_ */
