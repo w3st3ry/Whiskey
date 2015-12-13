83bbfbaa88ffff33f941829c6cd3184dfe68229e
Antoine Motet <motet_a@epitech.eu>
2015-12-13 15:27:55 +0100

    Clean up string_reader.c

--- string_reader.c
+++ string_reader.c
@@ -5,20 +5,25 @@
 #include "gc.h"
 
 
-wsky_StringReader wsky_StringReader_create(wsky_ProgramFile *file,
-					   const char *string) {
+typedef wsky_Token Token;
+typedef wsky_Position Position;
+typedef wsky_StringReader StringReader;
+
+
+StringReader wsky_StringReader_create(wsky_ProgramFile *file,
+				      const char *string) {
 
   wsky_XINCREF(file);
   if (!file && !string) {
     abort();
   }
-  wsky_Position pos = {
+  Position pos = {
     .index = 0,
     .line = 1,
     .column = 0,
     .file = file,
   };
-  wsky_StringReader reader = {
+  StringReader reader = {
     .file = file,
     .string = string,
     .position = pos,
@@ -26,35 +31,35 @@ wsky_StringReader wsky_StringReader_create(wsky_ProgramFile *file,
   return reader;
 }
 
-wsky_StringReader *wsky_StringReader_newFile(wsky_ProgramFile *file) {
+StringReader *wsky_StringReader_newFile(wsky_ProgramFile *file) {
   return wsky_StringReader_new(file, NULL);
 }
 
-wsky_StringReader *wsky_StringReader_new(wsky_ProgramFile *file,
-					 const char *string) {
-  wsky_StringReader *reader = malloc(sizeof(wsky_StringReader));
+StringReader *wsky_StringReader_new(wsky_ProgramFile *file,
+				    const char *string) {
+  StringReader *reader = malloc(sizeof(StringReader));
   if (!reader)
     return NULL;
   *reader = wsky_StringReader_create(file, string);
   return reader;
 }
 
-wsky_StringReader *wsky_StringReader_newStr(const char *string) {
+StringReader *wsky_StringReader_newStr(const char *string) {
   return wsky_StringReader_new(NULL, string);
 }
 
-void wsky_StringReader_delete(wsky_StringReader *reader) {
+void wsky_StringReader_delete(StringReader *reader) {
   wsky_XDECREF(reader->file);
   free(reader);
 }
 
 
 
-bool wsky_StringReader_hasMore(const wsky_StringReader *reader) {
+bool wsky_StringReader_hasMore(const StringReader *reader) {
   return (reader->position.index < (int)strlen(reader->string));
 }
 
-char wsky_StringReader_next(wsky_StringReader *reader) {
+char wsky_StringReader_next(StringReader *reader) {
   if (!wsky_StringReader_hasMore(reader))
     abort();
   char c = reader->string[reader->position.index++];
@@ -67,9 +72,8 @@ char wsky_StringReader_next(wsky_StringReader *reader) {
   return c;
 }
 
-bool wsky_StringReader_readString(wsky_StringReader *reader,
-				  const char *string) {
-  wsky_Position begin = reader->position;
+bool wsky_StringReader_readString(StringReader *reader, const char *string) {
+  Position begin = reader->position;
   while (*string) {
     if (!wsky_StringReader_hasMore(reader)) {
       reader->position = begin;
@@ -86,12 +90,10 @@ bool wsky_StringReader_readString(wsky_StringReader *reader,
   return true;
 }
 
-int wsky_StringReader_skip(wsky_StringReader *reader,
-			   const char *charsToSkip) {
-
+int wsky_StringReader_skip(StringReader *reader, const char *charsToSkip) {
   int count = 0;
   while (wsky_StringReader_hasMore(reader)) {
-    wsky_Position previous = reader->position;
+    Position previous = reader->position;
     char c = wsky_StringReader_next(reader);
     if (strchr(charsToSkip, c) == NULL) {
       reader->position = previous;
@@ -102,17 +104,17 @@ int wsky_StringReader_skip(wsky_StringReader *reader,
   return count;
 }
 
-int wsky_StringReader_skipWhitespaces(wsky_StringReader *reader) {
+int wsky_StringReader_skipWhitespaces(StringReader *reader) {
   return wsky_StringReader_skip(reader, " \t\n\r");
 }
 
-wsky_Token wsky_StringReader_createToken(wsky_StringReader *reader,
-					 wsky_Position begin,
-					 wsky_TokenType type) {
+Token wsky_StringReader_createToken(StringReader *reader,
+				    Position begin,
+				    wsky_TokenType type) {
   const char *stringBegin = reader->string + begin.index;
   int length = reader->position.index - begin.index;
   char *string = strndup(stringBegin, (unsigned)length);
-  wsky_Token t = wsky_Token_create(begin, reader->position, string, type);
+  Token t = wsky_Token_create(begin, reader->position, string, type);
   free(string);
   return  t;
 }
