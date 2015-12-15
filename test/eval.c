373bbf28907a319291a620281ff3051134085133
Antoine Motet <motet_a@epitech.eu>
2015-12-15 01:18:48 +0100

    Add tests in tests/eval.c

--- test/eval.c
+++ test/eval.c
@@ -1,7 +1,18 @@
 #include "tests.h"
 
 #include "eval.h"
+#include "exception.h"
 
 void evalTestSuite(void) {
-  wsky_evalString("123");
+  wsky_ReturnValue r;
+
+  r = wsky_evalString("123");
+  yolo_assert_null(r.exception);
+  yolo_assert(r.v.type == wsky_Type_INT);
+  yolo_assert(r.v.v.intValue == 123);
+
+  r = wsky_evalString("123.0");
+  yolo_assert_null(r.exception);
+  yolo_assert(r.v.type == wsky_Type_FLOAT);
+  yolo_assert(r.v.v.floatValue == 123.0);
 }
