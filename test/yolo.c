b58ff8840cac80e91e5fd5230fcaae7c9f7fad28
Antoine Motet <motet_a@epitech.eu>
2015-12-28 19:22:59 +0100

    Add colors to unit tests

--- test/yolo.c
+++ test/yolo.c
@@ -12,14 +12,22 @@ static void print_csi_sgr(int n) {
   printf("\x1b[%dm", n);
 }
 
+static void reset_attributes() {
+  print_csi_sgr(0);
+}
+
 static void print_fail(const char *test_name, const char *position) {
   printf("\n");
+
+  print_csi_sgr(95);
   printf("%s\n", position);
+  reset_attributes();
+
   print_csi_sgr(31);
   print_csi_sgr(1);
   printf("%s failed", test_name);
   printf(": ");
-  print_csi_sgr(0);
+  reset_attributes();
 }
 
 void yolo_fail_impl(const char *test_name, const char *position) {
@@ -67,17 +75,29 @@ void yolo_assert_impl(bool a, const char *test_name, const char *position) {
   }
 
 void yolo_assert_str_eq_impl(const char *expected, const char *result,
-                               const char *test_name,
-                               const char *position) {
-if (strcmp(expected, result) == 0)
-  yolo_assert_impl(true, test_name, position);
- else
-   {
-print_fail(test_name, position);
-printf("'%s' expected, got '%s' instead\n",
-         expected, result);
-yolo_state.fail_count++;
-}
+                             const char *test_name,
+                             const char *position) {
+  if (strcmp(expected, result) == 0)
+    yolo_assert_impl(true, test_name, position);
+  else
+    {
+      print_fail(test_name, position);
+
+      printf("'");
+
+      print_csi_sgr(96);
+      printf("%s", expected);
+      reset_attributes();
+
+      printf("' expected, got '");
+
+      print_csi_sgr(91);
+      printf("%s", result);
+      reset_attributes();
+
+      printf("' instead\n");
+      yolo_state.fail_count++;
+    }
 }
 
 
@@ -95,10 +115,26 @@ void yolo_begin(void) {
 }
 
 void yolo_end(void) {
-int total = yolo_state.fail_count + yolo_state.success_count;
-float percent = ((float) yolo_state.success_count / total) * 100;
-printf("\n%d tests, %d failure(s) (%.1f%%)\n",
-         total,
-         yolo_state.fail_count,
-         percent);
+  int total = yolo_state.fail_count + yolo_state.success_count;
+  float percent = ((float) yolo_state.success_count / total) * 100;
+
+  printf("\n");
+
+  print_csi_sgr(1);
+  printf("%d", total);
+  reset_attributes();
+
+  printf(" tests, ");
+
+  print_csi_sgr(1);
+  printf("%d", yolo_state.fail_count);
+  reset_attributes();
+
+  printf(" failure(s) (");
+
+  print_csi_sgr(1);
+  printf("%.1f%%", percent);
+  reset_attributes();
+
+  printf(")\n");
 }
