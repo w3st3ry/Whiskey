5425401140485a6821bf9039c9ce79b018d91151
Antoine Motet <motet_a@epitech.eu>
2015-12-29 21:13:54 +0100

    Add documentation in string_reader.h

--- include/string_reader.h
+++ include/string_reader.h
@@ -27,7 +27,7 @@ typedef struct wsky_StringReader_s {
 
 
 /**
- * Creates a reader
+ * Creates a reader.
  * @param file The file or NULL
  * @param string The string
  */
@@ -35,26 +35,46 @@ wsky_StringReader wsky_StringReader_create(wsky_ProgramFile *file,
                                            const char *string);
 
 /**
- * Frees the given reader
+ * Frees the given reader.
  */
 void wsky_StringReader_free(wsky_StringReader *reader);
 
-
+/**
+ * Reads a character.
+ * Increments the position of the reader.
+ */
 char wsky_StringReader_next(wsky_StringReader *reader);
 
-/** Returns true if the string has been read, or false otherwise */
+/**
+ * Reads the given string.
+ * Returns true if the string has been read, or false otherwise.
+ */
 bool wsky_StringReader_readString(wsky_StringReader *reader,
                                   const char *string);
 
-/* Returns the number of skipped characters */
+/**
+ * Skips characters.
+ * Returns the number of skipped characters.
+ * @param reader The reader
+ * @param charsToSkip A null-terminated string of the characters to skip.
+ */
 int wsky_StringReader_skip(wsky_StringReader *reader,
                            const char *charsToSkip);
 
-/* Returns the number of skipped characters */
+/**
+ * Skips line breaks, tabulations and spaces.
+ * Returns the number of skipped characters
+ */
 int wsky_StringReader_skipWhitespaces(wsky_StringReader *reader);
 
+/**
+ * Returns false if the reader is at the end of the string.
+ */
 bool wsky_StringReader_hasMore(const wsky_StringReader *reader);
 
+/**
+ * Creates a wsky_Token.
+ */
 wsky_Token wsky_StringReader_createToken(wsky_StringReader *reader,
                                          wsky_Position begin,
                                          wsky_TokenType type);
