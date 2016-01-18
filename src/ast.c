9f8a550eea7edc2b327d8a2034a1b1e89c2cf6b5
Antoine Motet <motet_a@epitech.eu>
2016-01-18 14:27:19 +0100

    Oops, I have forgotten a `!`...

--- src/ast.c
+++ src/ast.c
@@ -255,7 +255,7 @@ static char *intNodeToString(const LiteralNode *node) {
 static char *floatNodeToString(const LiteralNode *node) {
   wsky_Value value = wsky_Value_fromFloat(node->v.floatValue);
   wsky_ReturnValue stringRv = wsky_toString(value);
-  assert(stringRv.exception);
+  assert(!stringRv.exception);
   assert(wsky_isString(stringRv.v));
   wsky_String *string = (wsky_String *)stringRv.v.v.objectValue;
   return wsky_strdup(string->string);
