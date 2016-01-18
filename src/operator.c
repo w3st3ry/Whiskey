7568e2c47ad77a4b06f0192f9a73f98d5f9e600c
Antoine Motet <motet_a@epitech.eu>
2016-01-18 22:57:35 +0100

    Add cases in wsky_Operator_toString()

--- src/operator.c
+++ src/operator.c
@@ -1,18 +1,34 @@
 #include "operator.h"
 
 const char *wsky_Operator_toString(wsky_Operator operator) {
-# define CASE(name, op) case wsky_Operator_ ## name: return #op
+# define C(name, op) case wsky_Operator_ ## name: return #op
 
   switch (operator) {
-    CASE(PLUS, +); CASE(MINUS, -);
-    CASE(STAR, *); CASE(SLASH, /);
-    CASE(EQUALS, ==); CASE(NOT_EQUALS, !=);
-    CASE(LT, <); CASE(LT_EQ, <=);
-    CASE(GT, >); CASE(GT_EQ, >=);
 
-    CASE(AND, and); CASE(OR, or); CASE(NOT, not);
+  case wsky_Operator_LEFT_PAREN: return "(";
+  case wsky_Operator_RIGHT_PAREN: return ")";
+  case wsky_Operator_COMMA: return ",";
+
+    C(COLON, :); C(SEMICOLON, ;);
+    C(DOT, .);
+    C(LEFT_BRACKET, [); C(RIGHT_BRACKET, ]);
+    C(LEFT_BRACE, {); C(RIGHT_BRACE, });
+
+    C(PLUS, +); C(MINUS, -);
+    C(STAR, *); C(SLASH, /);
+
+    C(PLUS_EQ, +); C(MINUS_EQ, -);
+    C(STAR_EQ, *); C(SLASH_EQ, /);
+
+    C(EQUALS, ==); C(NOT_EQUALS, !=);
+    C(LT, <); C(LT_EQ, <=);
+    C(GT, >); C(GT_EQ, >=);
+
+    C(AND, and); C(OR, or); C(NOT, not);
+
+    C(ASSIGN, =);
   }
 
   return "op";
-# undef CASE
+# undef C
 }
