53916bbe0db12ea1553640d385375676ed6de21f
Antoine Motet <motet_a@epitech.eu>
2016-03-06 15:57:24 +0100

    test/program_file.c: The opened and read file is eval.c now since str.c has been removed.

--- test/program_file.c
+++ test/program_file.c
@@ -9,14 +9,14 @@ typedef wsky_ProgramFile ProgramFile;
 typedef wsky_ReturnValue ReturnValue;
 
 static void baseTests(void) {
-  char *filePath = getLocalFilePath("str.c");
+  char *filePath = getLocalFilePath("eval.c");
   ReturnValue rv = wsky_ProgramFile_new(filePath);
   wsky_free(filePath);
   yolo_assert_null(rv.exception);
   ProgramFile *pf = (ProgramFile *)rv.v.v.objectValue;
   yolo_assert_not_null(pf);
   yolo_assert_ulong_neq(0, strlen(pf->content));
-  yolo_assert_str_eq("str.c", pf->name);
+  yolo_assert_str_eq("eval.c", pf->name);
   if (!pf)
     return;
 
